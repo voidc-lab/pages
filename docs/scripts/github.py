7471e16fe386fe3fa297d974dd98be2be2111f85
Dmitry Borodkin <borodkin.dn@gmail.com>
2026-04-06 16:33:10 +0300

    Zensical!

---
+++ docs/scripts/github.py
@@ -0,0 +1,24 @@
+import re
+
+
+my_dict = {
+    "voidc_repo":     "https://github.com/voidc-lab/voidc",
+    "voidc_repo_url": "https://github.com/voidc-lab/voidc/blob/master",
+}
+
+def do_replace(match):
+
+    return my_dict.get(match.group(1), "???")
+
+
+pattern = re.compile(r"\{\{(\w+)\}\}")
+
+def do_line(line):
+
+    return pattern.sub(do_replace, line)
+
+
+def patch(lines):
+
+    return map(do_line, lines)
+
