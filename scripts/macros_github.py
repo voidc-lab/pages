e7f0c991b2a91018cc0a196c7e731c57cdb8eba1
Dmitry Borodkin <borodkin.dn@gmail.com>
2026-05-30 14:29:21 +0300

    Macros from zensical...

---
+++ scripts/macros_github.py
@@ -0,0 +1,4 @@
+def define_env(env):
+    env.variables["voidc_repo"]     = "https://github.com/voidc-lab/voidc"
+    env.variables["voidc_repo_url"] = "https://github.com/voidc-lab/voidc/blob/master"
+
---
+++ scripts/macros_gitlab.py
@@ -0,0 +1,4 @@
+def define_env(env):
+    env.variables["voidc_repo"]     = "https://gitlab.com/voidc-lab/voidc"
+    env.variables["voidc_repo_url"] = "https://gitlab.com/voidc-lab/voidc/-/blob/master"
+
